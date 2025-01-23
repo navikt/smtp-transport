2a2d65fc70635be6e50d9b18d7b8e1ed5b06e36d
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2025-01-23 16:16:05 +0100

    Removed unused dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,13 +13,9 @@ dependencyResolutionManagement {
         create("libs") {
             version("arrow", "1.2.4")
             version("suspendapp", "0.4.0")
-            version("bouncycastle", "1.76")
-            version("exposed", "0.47.0")
             version("ktor", "2.3.8")
             version("jakarta-mail", "2.1.2")
             version("eclipse-angus", "2.0.2")
-            version("jsch", "0.2.16")
-            version("cxf", "3.5.5")
             version("hoplite", "2.8.2")
             version("kotlin-kafka", "0.4.0")
             version("sqldelight", "2.0.2")
@@ -32,14 +28,8 @@ dependencyResolutionManagement {
             library("arrow-suspendapp", "io.arrow-kt", "suspendapp").versionRef("suspendapp")
             library("arrow-suspendapp-ktor", "io.arrow-kt", "suspendapp-ktor").versionRef("suspendapp")
 
-            library("bcpkix-jdk18on", "org.bouncycastle", "bcpkix-jdk18on").versionRef("bouncycastle")
-            library("bcprov-jdk18on", "org.bouncycastle", "bcprov-jdk18on").versionRef("bouncycastle")
-            library("apache-santuario", "org.apache.santuario:xmlsec:3.0.2")
-
             library("hoplite-core", "com.sksamuel.hoplite", "hoplite-core").versionRef("hoplite")
             library("hoplite-hocon", "com.sksamuel.hoplite", "hoplite-hocon").versionRef("hoplite")
-
-            library("jsch", "com.github.mwiede", "jsch").versionRef("jsch")
 
             library("kotlin-kafka", "io.github.nomisrev", "kotlin-kafka").versionRef("kotlin-kafka")
 
@@ -65,44 +55,28 @@ dependencyResolutionManagement {
             library("sqldelight-primitive-adapters", "app.cash.sqldelight", "primitive-adapters").versionRef("sqldelight")
 
             library("postgresql", "org.postgresql", "postgresql").versionRef("postgres")
-
             library("vault-jdbc", "no.nav", "vault-jdbc").versionRef("vault-jdbc")
-
             library("hikari", "com.zaxxer:HikariCP:5.0.1")
-            library("labai-jsr305x-annotations", "com.github.labai:labai-jsr305x-annotations:0.0.2")
-            library("jakarta.xml.bind-api", "jakarta.xml.bind:jakarta.xml.bind-api:4.0.0")
-            library("ebxml-protokoll", "no.nav.emottak:ebxml-protokoll:0.0.6")
+
             library("ktor-server-auth-jvm", "io.ktor:ktor-server-auth-jvm:2.3.8")
             library("token-validation-ktor-v2", "no.nav.security:token-validation-ktor-v2:4.1.4")
             library("flyway-core", "org.flywaydb:flyway-core:9.16.3")
-            library("emottak-payload-xsd", "no.nav.emottak:emottak-payload-xsd:0.0.6")
-            library("jaxb-runtime", "org.glassfish.jaxb:jaxb-runtime:2.4.0-b180830.0438")
-            library("cxf-rt-frontend-jaxws", "org.apache.cxf", "cxf-rt-frontend-jaxws").versionRef("cxf")
-            library("cxf-rt-transports-http", "org.apache.cxf", "cxf-rt-transports-http").versionRef("cxf")
-            library("cxf-rt-ws-security", "org.apache.cxf", "cxf-rt-ws-security").versionRef("cxf")
-            library("cxf-rt-features-logging", "org.apache.cxf", "cxf-rt-features-logging").versionRef("cxf")
-            library("jakarta.xml.ws-api", "jakarta.xml.ws:jakarta.xml.ws-api:2.3.3")
 
             library("jakarta-mail-api", "jakarta.mail", "jakarta.mail-api").versionRef("jakarta-mail")
             library("eclipse-angus", "org.eclipse.angus", "jakarta.mail").versionRef("eclipse-angus")
 
             bundle("prometheus", listOf("ktor-server-metrics-micrometer", "micrometer-registry-prometheus"))
-            bundle("cxf", listOf("cxf-rt-frontend-jaxws", "cxf-rt-transports-http", "cxf-rt-ws-security", "cxf-rt-features-logging", "jakarta.xml.ws-api"))
             bundle("jakarta-mail", listOf("jakarta-mail-api", "eclipse-angus"))
-            bundle("bouncycastle", listOf("bcpkix-jdk18on", "bcprov-jdk18on"))
             bundle("logging", listOf("logback-classic", "logback-logstash"))
         }
 
         create("testLibs") {
-            version("junit", "5.9.2")
-            version("hamcrest", "2.2")
-            version("mockk", "1.13.10")
             version("testPostgres", "1.18.0")
-            version("xmlunit", "2.9.1")
             version("ktor-server-test", "2.3.4")
             version("kotest", "5.9.1")
             version("testcontainers", "1.18.1")
             version("kotest-extensions", "2.0.2")
+            version("greenmail", "2.1.0-alpha-3")
             version("postgres", "42.7.4")
             version("turbine", "1.1.0") // Nedgradert fra 1.2.0 pga avhengigheten til
             //                             kotlinx-coroutines-core-jvm:1.9.0 som gjør at SmtpTransportIntegrationTest.kt
@@ -110,19 +84,11 @@ dependencyResolutionManagement {
             //                             LockFreeLinkedListHead.addLast(kotlinx.coroutines.internal.LockFreeLinkedListNode)
             version("arrow", "1.2.4")
 
+            library("greenmail", "com.icegreen", "greenmail").versionRef("greenmail")
+            library("greenmail-junit5", "com.icegreen", "greenmail-junit5").versionRef("greenmail")
+
             library("ktor-server-test-host", "io.ktor", "ktor-server-test-host").versionRef("ktor-server-test")
-            library("junit-jupiter-api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
-            library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
-            library("junit-jupiter-params", "org.junit.jupiter", "junit-jupiter-params").versionRef("junit")
             library("mock-oauth2-server", "no.nav.security:mock-oauth2-server:2.1.2")
-
-            library("hamcrest", "org.hamcrest", "hamcrest").versionRef("hamcrest")
-
-            library("mockk-jvm", "io.mockk", "mockk-jvm").versionRef("mockk")
-            library("mockk-dsl-jvm", "io.mockk", "mockk-dsl-jvm").versionRef("mockk")
-
-            library("xmlunit-core", "org.xmlunit", "xmlunit-core").versionRef("xmlunit")
-            library("xmlunit-matchers", "org.xmlunit", "xmlunit-matchers").versionRef("xmlunit")
 
             library("kotest-runner-junit5", "io.kotest", "kotest-runner-junit5").versionRef("kotest")
             library("kotest-framework-datatest", "io.kotest", "kotest-framework-datatest").versionRef("kotest")
@@ -139,38 +105,13 @@ dependencyResolutionManagement {
 
             library("turbine", "app.cash.turbine", "turbine").versionRef("turbine")
 
+            bundle("greenmail", listOf("greenmail", "greenmail-junit5"))
             bundle("kotest", listOf("kotest-runner-junit5", "kotest-framework-datatest"))
-            bundle("mockk", listOf("mockk-jvm", "mockk-dsl-jvm"))
-            bundle("xmlunit", listOf("xmlunit-core", "xmlunit-matchers"))
         }
     }
 
     repositories {
         mavenCentral()
-        maven {
-            name = "Ebxml protokoll"
-            url = uri("https://maven.pkg.github.com/navikt/ebxml-protokoll")
-            credentials {
-                username = "token"
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-        maven {
-            name = "Emottak payload format"
-            url = uri("https://maven.pkg.github.com/navikt/emottak-payload-xsd")
-            credentials {
-                username = "token"
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-        maven {
-            name = "Emottak ebms model"
-            url = uri("https://maven.pkg.github.com/navikt/ebxml-processor")
-            credentials {
-                username = "token"
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
         maven {
             name = "Nav token-support"
             url = uri("https://maven.pkg.github.com/navikt/token-support")
