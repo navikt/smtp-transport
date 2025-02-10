05e5d3c6c46df9a662cb2b76b8aae0928a9f42ce
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2025-02-10 13:53:17 +0100

    Merging payload and signal flows

--- src/main/kotlin/no/nav/emottak/processor/MessageProcessor.kt
+++ src/main/kotlin/no/nav/emottak/processor/MessageProcessor.kt
@@ -4,8 +4,10 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onEach
 import no.nav.emottak.log
+import no.nav.emottak.model.Message
 import no.nav.emottak.model.PayloadMessage
 import no.nav.emottak.model.SignalMessage
 import no.nav.emottak.receiver.PayloadReceiver
@@ -15,30 +17,21 @@ class MessageProcessor(
     private val payloadReceiver: PayloadReceiver,
     private val signalReceiver: SignalReceiver
 ) {
-    fun processPayloadAndSignalMessages(scope: CoroutineScope) {
-        processPayloadMessages(scope)
-        processSignalMessages(scope)
+    fun processPayloadAndSignalMessages(scope: CoroutineScope) = process(scope)
+
+    private fun process(scope: CoroutineScope) =
+        merge(
+            payloadReceiver.receivePayloadMessages(),
+            signalReceiver.receiveSignalMessages()
+        )
+            .onEach(::processMessage)
+            .flowOn(Dispatchers.IO)
+            .launchIn(scope)
+
+    private fun processMessage(message: Message) {
+        when (message) {
+            is PayloadMessage -> log.info("Processed payload message with reference id: ${message.messageId}")
+            is SignalMessage -> log.info("Processed signal message with reference id: ${message.messageId}")
+        }
     }
-
-    private fun processPayloadMessages(scope: CoroutineScope) =
-        payloadReceiver
-            .receivePayloadMessages()
-            .onEach(::processPayloadMessage)
-            .flowOn(Dispatchers.IO)
-            .launchIn(scope)
-
-    private fun processSignalMessages(scope: CoroutineScope) =
-        signalReceiver
-            .receiveSignalMessages()
-            .onEach(::processSignalMessage)
-            .flowOn(Dispatchers.IO)
-            .launchIn(scope)
-}
-
-private fun processPayloadMessage(payloadMessage: PayloadMessage) {
-    log.info("Processed payload message with reference id: ${payloadMessage.messageId}")
-}
-
-private fun processSignalMessage(signalMessage: SignalMessage) {
-    log.info("Processed signal message with reference id: ${signalMessage.messageId}")
 }
