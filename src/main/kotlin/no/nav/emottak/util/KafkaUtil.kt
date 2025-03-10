79abdccce72893523470e5fc64d3c5e20e7b0031
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2025-03-10 12:27:37 +0100

    More verbose logging

--- src/main/kotlin/no/nav/emottak/util/KafkaUtil.kt
+++ src/main/kotlin/no/nav/emottak/util/KafkaUtil.kt
@@ -5,6 +5,6 @@ import no.nav.emottak.log
 
 fun <K, V> ReceiverRecord<K, V>.getHeaderValueAsString(value: String): String =
     when (val header = headers().lastHeader(value)) {
-        null -> "".also { log.warn("Header missing: $value") }
+        null -> "".also { log.warn("Kafka header missing: $value") }
         else -> String(header.value())
     }
