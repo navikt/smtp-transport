99b895dcbd81c1997693d6b5f4218dc9a3cb3611
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2025-05-16 12:32:31 +0200

    Logging 404 as warnings

--- src/main/kotlin/no/nav/emottak/plugin/Logging.kt
+++ src/main/kotlin/no/nav/emottak/plugin/Logging.kt
@@ -1,16 +1,18 @@
 package no.nav.emottak.plugin
 
+import io.ktor.http.HttpStatusCode.Companion.NotFound
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.plugins.calllogging.processingTimeMillis
 import io.ktor.server.request.httpMethod
 import io.ktor.server.request.path
-import org.slf4j.event.Level
+import org.slf4j.LoggerFactory
 
 fun Application.configureCallLogging() {
+    val logger = LoggerFactory.getLogger("CallLogging")
+
     install(CallLogging) {
-        level = Level.INFO
         filter { call -> call.request.path().startsWith("/api") }
         format { call ->
             val status = call.response.status()
@@ -19,7 +21,20 @@ fun Application.configureCallLogging() {
             val path = call.request.path()
             val duration = call.processingTimeMillis()
 
-            "Status: $status, Method: $httpMethod, Path: $path, User agent: $userAgent, Duration: $duration ms"
+            val message = """
+                Status: $status
+                Method: $httpMethod
+                Path: $path
+                User-Agent: $userAgent
+                Duration: ${duration}ms
+            """.trimIndent()
+
+            when (status) {
+                NotFound -> logger.warn(message)
+                else -> logger.info(message)
+            }
+
+            ""
         }
     }
 }
