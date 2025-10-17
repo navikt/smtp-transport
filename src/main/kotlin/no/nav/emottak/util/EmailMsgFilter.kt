c1c7d775d3cd85fb727641353d9d497b72fdb5a1
Chris Olsen <christopher.olsen@nav.no>
2025-10-17 11:12:29 +0200

    Refactor filterMessage

--- src/main/kotlin/no/nav/emottak/util/EmailMsgFilter.kt
+++ src/main/kotlin/no/nav/emottak/util/EmailMsgFilter.kt
@@ -1,27 +1,25 @@
 package no.nav.emottak.util
 
 import no.nav.emottak.config
+import no.nav.emottak.log
 import no.nav.emottak.smtp.EmailMsg
 
 fun EmailMsg.filterMimeMessage(): ForwardingSystem {
-    val from = this.headers["From"] ?: ""
-    val subject = this.headers["Subject"] ?: ""
-    return if (checkFromAddress(from)) {
-        if (checkSignalMessage(subject)) {
-            ForwardingSystem.BOTH
-        } else if (checkMessageType(subject)) {
-            ForwardingSystem.EBMS
-        } else {
-            ForwardingSystem.EMOTTAK
+    val from = this.headers["From"] ?: "".also { log.warn("No [From] header found") }
+    val subject = this.headers["Subject"] ?: "".also { log.warn("No [Subject] header found") }
+
+    if(from.isNotBlank() && subject.isNotBlank()) {
+        if (isFromAcceptedAddress(from)) {
+            if (isSignalMessage(subject)) return ForwardingSystem.BOTH
+            if (isAcceptedType(subject)) return ForwardingSystem.EBMS
         }
-    } else {
-        ForwardingSystem.EMOTTAK
     }
+    return ForwardingSystem.EMOTTAK
 }
 
-private fun checkMessageType(subject: String) = config().ebmsFilter.ebmsMessageTypeSubjects.contains(subject.lowercase())
-private fun checkSignalMessage(subject: String) = config().ebmsFilter.signalMessageTypeSubjects.contains(subject.lowercase())
-private fun checkFromAddress(from: String) = config().ebmsFilter.ebmsSenderAddresses.contains(from.lowercase())
+private fun isAcceptedType(subject: String) = config().ebmsFilter.ebmsMessageTypeSubjects.contains(subject.lowercase())
+private fun isSignalMessage(subject: String) = config().ebmsFilter.signalMessageTypeSubjects.contains(subject.lowercase())
+private fun isFromAcceptedAddress(from: String) = config().ebmsFilter.ebmsSenderAddresses.contains(from.lowercase())
 
 enum class ForwardingSystem {
     EBMS, EMOTTAK, BOTH
