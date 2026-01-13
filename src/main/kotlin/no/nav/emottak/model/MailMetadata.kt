3c1be7d11dcb83cd5844a3fed32c838de9e54961
Thomas Burnett <thomas.hoberg.burnett@nav.no>
2026-01-13 17:04:19 +0100

    Henter avsenderepost fra kafka header (#50)
    * Get emottak sender address from kafka header. Fallback to value from configuration.
    
    * Update const value
    
    * La til tester som sjekker mime headers
    
    * ktlint
    
    * Test for expunging messages
    
    ---------
    
    Co-authored-by: Thomas Burnett <thburnett@users.noreply.github.com>

--- src/main/kotlin/no/nav/emottak/model/MailMetadata.kt
+++ src/main/kotlin/no/nav/emottak/model/MailMetadata.kt
@@ -1,6 +1,7 @@
 package no.nav.emottak.model
 
 data class MailMetadata(
-    val addresses: String,
-    val subject: String = ""
+    val recipientAddress: String,
+    val subject: String = "",
+    val senderAddress: String
 )
