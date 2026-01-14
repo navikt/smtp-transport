acba7e1ebdfe7fd6947c56d7a10560b83e540c04
Thomas Burnett <thomas.hoberg.burnett@nav.no>
2026-01-14 10:21:39 +0100

    Splitter filter config for dev og prod (#54)
    * Splitter filter config for dev og prod
    
    * Setter avsender epost til nyebms istedenfor qass
    
    * Formatting fix
    
    * Added logging of filter configuration file name
    
    ---------
    
    Co-authored-by: Thomas Burnett <thburnett@users.noreply.github.com>

--- src/main/kotlin/no/nav/emottak/Configurator.kt
+++ src/main/kotlin/no/nav/emottak/Configurator.kt
@@ -5,6 +5,7 @@ import com.sksamuel.hoplite.ConfigLoader
 import com.sksamuel.hoplite.ExperimentalHoplite
 import com.sksamuel.hoplite.addResourceSource
 import no.nav.emottak.configuration.Config
+import no.nav.emottak.utils.environment.getEnvVar
 
 @OptIn(ExperimentalHoplite::class)
 val config: () -> Config = {
@@ -12,8 +13,17 @@ val config: () -> Config = {
         .addResourceSource("/application-personal.conf", optional = true)
         .addResourceSource("/kafka_common.conf")
         .addResourceSource("/application.conf")
+        .addResourceSource(filterConfigResolver())
         .withExplicitSealedTypes()
         .build()
         .loadConfigOrThrow<Config>()
 }
     .memoize()
+
+private fun filterConfigResolver() = when (getEnvVar("NAIS_CLUSTER_NAME", "local")) {
+    "prod-fss" -> "/filter-prod.conf"
+    "dev-fss" -> "/filter-dev.conf"
+    else -> "/filter-dev.conf"
+}.also {
+    log.info("Loading filter configuration from $it")
+}
