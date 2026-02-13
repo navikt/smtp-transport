63f1d62e1b3248abbeff075f1dea1db0e32bc62b
terjenilssen <terje.nilssen@nav.no>
2026-02-13 13:10:05 +0100

    Oppdatert til ConfluentKafkaContainer.

--- src/test/kotlin/no/nav/emottak/KafkaSpec.kt
+++ src/test/kotlin/no/nav/emottak/KafkaSpec.kt
@@ -17,7 +17,7 @@ import org.apache.kafka.common.serialization.ByteArrayDeserializer
 import org.apache.kafka.common.serialization.ByteArraySerializer
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
-import org.testcontainers.containers.KafkaContainer
+import org.testcontainers.kafka.ConfluentKafkaContainer
 import org.testcontainers.utility.DockerImageName
 import java.util.Properties
 import kotlin.time.Duration.Companion.seconds
@@ -32,12 +32,12 @@ abstract class KafkaSpec(body: KafkaSpec.() -> Unit = {}) : StringSpec() {
     private val consumerPollingTimeout = 1.seconds
 
     private val kafkaImage: DockerImageName =
-        DockerImageName.parse("confluentinc/cp-kafka:7.4.0")
+        DockerImageName.parse("confluentinc/cp-kafka:7.5.0")
 
-    internal val container: KafkaContainer =
+    internal val container: ConfluentKafkaContainer =
         install(
             ContainerExtension(
-                KafkaContainer(kafkaImage)
+                ConfluentKafkaContainer(kafkaImage)
                     .withExposedPorts(9092, 9093)
                     .withNetworkAliases("broker")
                     .withEnv("KAFKA_HOST_NAME", "broker")
