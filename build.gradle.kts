858fb9604ba2f21e51d133b8fbc72de44f1f2bc2
Thomas Burnett <thburnett@users.noreply.github.com>
2026-02-16 12:50:54 +0100

    Dependency order

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     kotlin("jvm") version "2.1.10"
     kotlin("plugin.serialization") version "2.1.10"
     id("io.ktor.plugin") version "3.3.1"
-    id("app.cash.sqldelight") version "2.0.2"
+    id("app.cash.sqldelight") version "2.2.1"
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
     id("com.gradleup.shadow") version "8.3.6"
 }
@@ -25,6 +25,7 @@ sqldelight {
 tasks {
     shadowJar {
         archiveFileName.set("app.jar")
+        mergeServiceFiles()
     }
     test {
         useJUnitPlatform()
