4e0b06e354bbc5004dc37e47f8243291c0486736
Thomas Burnett <thburnett@users.noreply.github.com>
2026-04-21 14:25:45 +0200

    ktlint

--- src/main/kotlin/no/nav/emottak/plugin/Routes.kt
+++ src/main/kotlin/no/nav/emottak/plugin/Routes.kt
@@ -12,7 +12,6 @@ import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
-import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
