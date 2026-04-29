3b942c9ee511829b99b22d0ff6ecdc49440b35d1
Chris (RettIProd) Olsen <christopher.olsen@nav.no>
2026-04-29 16:20:57 +0200

    Retry Connection

--- src/main/kotlin/no/nav/emottak/util/EbmsAsyncClient.kt
+++ src/main/kotlin/no/nav/emottak/util/EbmsAsyncClient.kt
@@ -4,8 +4,11 @@ import arrow.core.raise.Raise
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import no.nav.emottak.InvalidReferenceId
 import no.nav.emottak.PayloadError
 import no.nav.emottak.PayloadNotFound
@@ -18,7 +21,7 @@ class EbmsAsyncClient(
     private val httpClient: HttpClient
 ) {
     suspend fun Raise<PayloadError>.getPayloads(referenceId: Uuid): List<Payload> {
-        val httpResponse = httpClient.get("$referenceId")
+        val httpResponse = retryGetPayload(referenceId)
         return when (httpResponse.status) {
             HttpStatusCode.OK -> httpResponse.body<List<Payload>>()
             HttpStatusCode.NotFound -> raise(PayloadNotFound(referenceId.toString()))
@@ -26,5 +29,22 @@ class EbmsAsyncClient(
             HttpStatusCode.Unauthorized -> raise(UnauthorizedError)
             else -> raise(UnknownError(httpResponse.bodyAsText()))
         }
+    }
+
+    suspend fun retryGetPayload(referenceId: Uuid): HttpResponse {
+        val attemptLimit = 30
+        val delayMillis = 5000L
+        var attempt = 0
+        while (attemptLimit > attempt) {
+            try {
+                return httpClient.get("$referenceId")
+            } catch (e: Exception) {
+                withContext(Dispatchers.IO) {
+                    Thread.sleep(delayMillis)
+                }
+            }
+            attempt++
+        }
+        throw Exception("Failed to get payload after $attemptLimit attempts")
     }
 }
