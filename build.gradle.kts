fafeb761661f8e34de7dc37d8f09d988748b8868
Thomas Burnett <thburnett@users.noreply.github.com>
2026-05-12 16:21:49 +0200

    Update Gradle wrapper to 9.5.0 and kotlin to 2.3.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     application
-    kotlin("jvm") version "2.1.10"
-    kotlin("plugin.serialization") version "2.1.10"
+    kotlin("jvm") version "2.3.21"
+    kotlin("plugin.serialization") version "2.3.21"
     id("io.ktor.plugin") version "3.3.1"
     id("app.cash.sqldelight") version "2.0.2"
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
