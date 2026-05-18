49c3be6f2ed60a84b052fcc0c5549aa506af839b
Thomas Burnett <thburnett@users.noreply.github.com>
2026-05-18 16:05:35 +0200

    Bump ktor to 3.5.0 and kotlin to 2.3.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     application
-    kotlin("jvm") version "2.1.10"
-    kotlin("plugin.serialization") version "2.1.10"
-    id("io.ktor.plugin") version "3.3.1"
+    kotlin("jvm") version "2.3.21"
+    kotlin("plugin.serialization") version "2.3.21"
+    id("io.ktor.plugin") version "3.5.0"
     id("app.cash.sqldelight") version "2.0.2"
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
     id("com.gradleup.shadow") version "8.3.6"
