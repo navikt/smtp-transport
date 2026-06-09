0e79ec4b9c7eef10f6739715c4d152316f3924fe
Aage Kramarics <Aage.Kramarics@nav.no>
2026-06-09 11:19:08 +0200

    Rullet tilbake logg-endring

--- src/main/kotlin/no/nav/emottak/util/EmailMsgFilter.kt
+++ src/main/kotlin/no/nav/emottak/util/EmailMsgFilter.kt
@@ -68,7 +68,7 @@ private fun EmailMsg.computeForwardingDecision(): ForwardingDecision {
             "sourceSystem" to (this.headers["X-Mailer"] ?: "-")
         )
     )
-    log.info(marker, "Message forwarding system identified: $forwardingSystem")
+    log.info(marker, "Message forwarding system identified")
 
     return ForwardingDecision(forwardingSystem, envelopeServiceName, envelopeCpaId, envelopeAction)
 }
