de5e935a3c7e81556b59070b15ee136f9df404b6
Thomas Burnett <thburnett@users.noreply.github.com>
2026-09-11 10:59:23 +0200

    Konfigurasjon for fase 1 av HarBorgerFrikortMengde

--- src/test/kotlin/no/nav/emottak/ConfiguratorSpec.kt
+++ src/test/kotlin/no/nav/emottak/ConfiguratorSpec.kt
@@ -161,13 +161,14 @@ class ConfiguratorSpec : StringSpec({
         services.map { it.name } shouldContain "Trekkopplysning"
         services.map { it.name } shouldContain "Sykmelding"
         services.map { it.name } shouldContain "Legemelding"
+        services.map { it.name } shouldContain "HarBorgerFrikortMengde"
         services.single { it.name == "urn:oasis:names:tc:ebxml-msg:service" }
             .forwardTo shouldBe ForwardingSystem.BOTH
     }
 
     "prod filter CPA lists are resolved from file and differ from dev" {
         val prodRules = prodConfig.services.toServiceRules()
-        prodRules.size shouldBe 6
+        prodRules.size shouldBe 7
         prodRules["urn:oasis:names:tc:ebxml-msg:service"]!!.allCpaIds.shouldBeTrue()
         prodRules["urn:oasis:names:tc:ebxml-msg:service"]!!.cpaIds.shouldBeEmpty()
         prodRules["Inntektsforesporsel"]!!.allCpaIds.shouldBeTrue()
@@ -180,18 +181,21 @@ class ConfiguratorSpec : StringSpec({
         prodRules["Sykmelding"]!!.cpaIds shouldContain "nav:112931"
         prodRules["Legemelding"]!!.allCpaIds.shouldBeFalse()
         prodRules["Legemelding"]!!.cpaIds shouldContain "nav:112935"
+        prodRules["HarBorgerFrikortMengde"]!!.allCpaIds.shouldBeFalse()
+        prodRules["HarBorgerFrikortMengde"]!!.cpaIds shouldContain "883992342_889640782_011"
     }
 
     "prod filter routes expected services to EBMS" {
         val toEbms = prodConfig.services
             .filter { it.forwardTo == ForwardingSystem.EBMS }
             .map { it.name }
-        toEbms.size shouldBe 5
+        toEbms.size shouldBe 6
         toEbms shouldContain "Inntektsforesporsel"
         toEbms shouldContain "Trekkopplysning"
         toEbms shouldContain "PasientlisteForesporsel"
         toEbms shouldContain "Sykmelding"
         toEbms shouldContain "Legemelding"
+        toEbms shouldContain "HarBorgerFrikortMengde"
     }
 
     "prod filter routes expected services to BOTH" {
