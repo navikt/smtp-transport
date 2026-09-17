09cd91ed89240fb53627ddebcdfab9766379fa75
Chris (RettIProd) Olsen <christopher.olsen@nav.no>
2026-09-17 12:50:19 +0200

    Tests

--- src/main/kotlin/no/nav/emottak/configuration/Config.kt
+++ src/main/kotlin/no/nav/emottak/configuration/Config.kt
@@ -198,11 +198,15 @@ data class ServiceFilter(
     val whitelist: String? = null,
     val blacklist: String? = null,
     val selection: String =
-        if (!whitelist.isNullOrBlank()) // exists
+        if (!whitelist.isNullOrBlank()) {
+            // exists
             "NONE"
-        else if (!blacklist.isNullOrBlank()) // exists
+        } else if (!blacklist.isNullOrBlank()) {
+            // exists
             "ALL"
-        else throw IllegalStateException("Config for filter $name Selection verdi ikke satt implisitt eller eksplisitt")
+        } else {
+            throw IllegalStateException("Config for filter $name Selection verdi ikke satt implisitt eller eksplisitt")
+        }
 )
 
 data class CleanupPayloadsJob(
