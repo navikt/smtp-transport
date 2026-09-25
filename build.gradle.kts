77c7cae5ba4e0aae457748280ade5ebb053b9046
Thomas Burnett <7643677+thburnett@users.noreply.github.com>
2026-09-25 11:34:56 +0200

    Bump ktor to 3.5.2 (#181)
    Co-authored-by: Thomas Burnett <thburnett@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     val kotlinVersion = "2.4.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    id("io.ktor.plugin") version "3.5.1"
+    id("io.ktor.plugin") version "3.5.2"
     id("app.cash.sqldelight") version "2.0.2"
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
     id("com.gradleup.shadow") version "9.2.2"
